7d4cdbd2daf1b0ba8f35bb50b9b39412afb5db6e
Thomas <cheng.hao.0825@outlook.com>
2016-07-06 22:56:09 +0800

    homework

---
+++ DayOneHomeWork/DayOneHomeWork/Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOneHomeWork
+{
+    public class Order
+    {
+        public enum ResultSumBy
+        {
+            Cost, Revenue
+        }
+        public static int[] GetGroupSum(IEnumerable<Order> orders,  int groupBy, ResultSumBy resultSumBy  )
+        {
+            var  result=new List<int>();
+            int counter = 1;
+            int tempSum = 0;
+            foreach(var order in orders)
+            {
+                if(resultSumBy == ResultSumBy.Cost)
+                {
+                    tempSum += order.Cost;
+                } else if(resultSumBy == ResultSumBy.Revenue) {
+                    tempSum += order.Revenue;
+                }
+
+                if(counter / groupBy == 1)
+                {
+                    result.Add(tempSum);
+                    counter = 1;
+                    tempSum = 0;
+                    continue;
+                }
+                counter++;
+            }
+            if(tempSum != 0) { result.Add(tempSum); }
+            return result.ToArray<int>();
+        }
+ 
+        public Order(int id,int cost , int revenue , int sellprice)
+        {
+            ID = id;
+            Cost = cost;
+            Revenue = revenue;
+            SellPrice = sellprice;              
+        }
+
+        public int Cost { get; private set; }
+        public int ID { get; private set; }
+        public int Revenue { get; private set; }
+        public int SellPrice { get; private set; }
+    }
+}
---
+++ DayOneHomeWork/DayOneHomeWorkTests/OrderTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayOneHomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+namespace DayOneHomeWork.Tests
+{
+    [TestClass()]
+    public class OrderTests
+    {
+        private IEnumerable<Order> GetInputDataSet = new List<Order>()
+        {
+           new Order(1  ,1  ,11,21),
+            new Order(2 ,2  ,12,22),
+            new Order(3 ,3  ,13,23),
+            new Order(4 ,4  ,14,24),
+            new Order(5 ,5  ,15,25),
+            new Order(6 ,6  ,16,26),
+            new Order(7 ,7  ,17,27),
+            new Order(8 ,8  ,18,28),
+            new Order(9 ,9  ,19,29),
+            new Order(10,10,20,30),
+            new Order(11,11,21,31)
+        };
+        [TestMethod()]
+        public void GetGroupSum_GroupBy_3_SumByCost_should_be_6_15_24_21()
+        {
+            //arrange
+            IEnumerable<Order> input = GetInputDataSet;
+            int groupBy = 3;
+            int[] expected ={ 6, 15, 24, 21 };
+
+            //act
+            var actual = Order.GetGroupSum(input, groupBy, Order.ResultSumBy.Cost);
+
+            //assetra
+            expected.ToExpectedObject().ShouldEqual(actual);
+            
+        }
+        [TestMethod()]  
+        public void GetGroupSum_GroupBy_4_SumByRevenue_should_be_50_66_60()
+        {
+            //arrange
+            var input = GetInputDataSet;
+            int groupBy = 4;
+            int[] expected = { 50, 66, 60 };
+
+            //act
+            var actual = Order.GetGroupSum(input, groupBy, Order.ResultSumBy.Revenue);
+            //assert
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+}
