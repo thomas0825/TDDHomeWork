23abbe0157deabc7ccd9079d54252d88ddb26b05
Thomas <cheng.hao.0825@outlook.com>
2016-07-13 16:55:23 +0800

    Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元
    RED

---
+++ DayTwoHomeWork/DayTwoHomeWork/order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DayTwoHomeWork
+{
+    public class Book
+    {
+        
+        public Book(string name, double price ,int quantity )
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+        public int Quantity { get; private set; }
+    }
+    public class order
+    {
+        private List<Book> _books;
+
+        public order(List<Book> books)
+        {
+            this._books = books;
+        }
+        public double GetOrderTotalAmount()
+        {
+            double TotalAmount = 0;
+            return TotalAmount;
+        }
+
+    }
+}
---
+++ DayTwoHomeWork/DayTwoHomeWorkTests/orderTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DayTwoHomeWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExpectedObjects;
+
+namespace DayTwoHomeWork.Tests
+{
+    [TestClass()]
+    public class orderTests
+    {
+        [TestMethod()]
+        public void GetOrderTotalAmountTest_Buy_1_Book_TotalAmount_ShouldBe_100()
+        {
+            //arrange 
+            List<Book> buyList = new List<Book>()
+            {
+                new Book(name:"1",price:100,quantity:1),
+                new Book(name:"2",price:100,quantity:0),
+                new Book(name:"3",price:100,quantity:0),
+                new Book(name:"4",price:100,quantity:0),
+                new Book(name:"5",price:100,quantity:0)
+            };
+            var target = new order(buyList);
+
+            //act
+            var acutal = target.GetOrderTotalAmount();
+            //assert
+            var expected = 100;
+            expected.ToExpectedObject().ShouldEqual(acutal);
+        }
+    }
+}
