a84d2688d0ec4a4bc1b14fcdb08b2bb7c055a80e
Thomas <cheng.hao.0825@outlook.com>
2016-07-13 19:38:28 +0800

    Scenario: 一次買了整套，一二三四五集各買了一本，價格應為100*5*0.75=375  Given 第一集買了 1 本  And 第二集買了 1 本  And 第三集買了 1 本  And 第四集買了 1 本  And 第五集買了 1 本  When 結帳 Then 價格應為 375 元
    Green

--- DayTwoHomeWork/DayTwoHomeWorkTests/orderTests.cs
+++ DayTwoHomeWork/DayTwoHomeWorkTests/orderTests.cs
@@ -82,5 +82,25 @@ namespace DayTwoHomeWork.Tests
             var expected = 320.0;
             expected.ToExpectedObject().ShouldEqual(acutal);
         }
+        [TestMethod()]
+        public void GetOrderTotalAmountTest_Buy_1_Book1_and_1_Book2_and_1_Book3_and_1_Book4_and_1_Book5_TotalAmount_ShouldBe_375()
+        {
+            //arrange 
+            List<Book> buyList = new List<Book>()
+            {
+                new Book(name:"1",price:100),
+                new Book(name:"2",price:100),
+                new Book(name:"3",price:100),
+                new Book(name:"4",price:100),
+                new Book(name:"5",price:100)
+            };
+            var target = new order(buyList);
+
+            //act
+            var acutal = target.GetOrderTotalAmount();
+            //assert
+            var expected = 375.0;
+            expected.ToExpectedObject().ShouldEqual(acutal);
+        }
     }
 }
